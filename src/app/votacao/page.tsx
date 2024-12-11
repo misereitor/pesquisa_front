4696b0816fe8a47ec8c0564cbb977948986d0cc5
Misael Miranda <misaelbahia1@hotmail.com>
2024-12-11 10:32:03 -0300

    update vercel

--- src/app/votacao/page.tsx
+++ src/app/votacao/page.tsx
@@ -6,13 +6,24 @@ import { getAllVotesByUser } from '@/service/voting-service';
 import { cookies } from 'next/headers';
 import Link from 'next/link';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Votacao() {
+  const cookieStore = await cookies();
+  const userCookies = cookieStore.get('user');
+
+  if (!userCookies) {
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <h2>Usuário não autenticado</h2>
+        <Link href="/">Clique aqui para retornar</Link>
+      </div>
+    );
+  }
+
   const categories = await getAllCategories();
   const companies = await getAllCompany();
   const userVotes = await getAllVotesByUser();
-  const cookieStore = await cookies();
-  const userCookies = cookieStore.get('user');
-  if (!userCookies) return (location.href = '/');
   const user: UserVote = JSON.parse(userCookies?.value);
   Promise.all([categories, companies, userVotes]);
 
