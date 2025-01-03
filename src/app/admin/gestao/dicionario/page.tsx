79bcdda80ce4e512712c35ec64b021fb08f126f4
Misael Miranda <misaelbahia1@hotmail.com>
2025-01-03 10:57:14 -0300

    update dictionary

--- src/app/admin/gestao/dicionario/page.tsx
+++ src/app/admin/gestao/dicionario/page.tsx
@@ -1,21 +1,29 @@
 'use client';
 
 import Dictionary from '@/components/dictionary/dictionary';
+import { Company } from '@/model/company';
 import { DictionaryEntry } from '@/model/dictionary';
+import { getAllCompany } from '@/service/company-service';
 import { getAllDictionaryService } from '@/service/dictionary';
 import { useEffect, useState } from 'react';
 
 export default function Dicionario() {
   const [dictionary, setDictionary] = useState<DictionaryEntry[]>([]);
-
+  const [companies, setCompanies] = useState<Company[]>([]);
   useEffect(() => {
     const getData = async () => {
       const data = await getAllDictionaryService();
       setDictionary(data.data);
+      const data2 = await getAllCompany();
+      setCompanies(data2);
     };
     getData();
   }, []);
   return (
-    <Dictionary dictionaryData={dictionary} setDictionary={setDictionary} />
+    <Dictionary
+      dictionaryData={dictionary}
+      companies={companies}
+      setDictionary={setDictionary}
+    />
   );
 }
--- src/components/dictionary/dictionary.tsx
+++ src/components/dictionary/dictionary.tsx
@@ -1,16 +1,27 @@
+'use client';
 import { DictionaryEntry } from '@/model/dictionary';
 import { Dispatch, SetStateAction, useState } from 'react';
 import ListDictionary from './list-dictionary';
 import FilterDictionary from './filter-dictionary';
+import { Company } from '@/model/company';
+import InputAutocomplete from '../input/input-autocomplete';
 
 type Props = {
   dictionaryData: DictionaryEntry[];
   setDictionary: Dispatch<SetStateAction<DictionaryEntry[]>>;
+  companies: Company[];
 };
 
-export default function Dictionary({ dictionaryData, setDictionary }: Props) {
+export default function Dictionary({
+  dictionaryData,
+  setDictionary,
+  companies
+}: Props) {
   const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState('');
+  const [companySelected, setCompanySelected] = useState<Company>();
 
+  const handleSubmit = async () => {};
   return (
     <div>
       <FilterDictionary
@@ -19,6 +30,18 @@ export default function Dictionary({ dictionaryData, setDictionary }: Props) {
         dictionaryData={dictionaryData}
         setDictionary={setDictionary}
       />
+      <div className="w-full flex items-center justify-center pl-20 bg-neutral-800 h-16">
+        <InputAutocomplete
+          setValue={setValue}
+          value={value}
+          handleSubmit={handleSubmit}
+          options={companies}
+          companySelected={companySelected}
+          setCompanySelected={setCompanySelected}
+          dictionaryFromService={dictionaryData}
+          name="outros"
+        />
+      </div>
       <div className="p-4 space-y-4 bg-neutral-700">
         <table className="w-full border-collapse text-sm">
           <thead className="bg-neutral-700 text-left border-b-2 border-black">
