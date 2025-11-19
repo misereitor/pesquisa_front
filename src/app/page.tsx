4e4b7cdd1db188e71e7e7c1c13be0b429acd47da
Misael Miranda <misaelbahia1@hotmail.com>
2025-11-19 14:22:55 -0300

    update pesquisa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 import Final from '@/components/final/final';
-//import LoginVoting from '../components/login-voting/page';
+import LoginVoting from '../components/login-voting/page';
 import { deleteCookie } from 'cookies-next/client';
 
 export default function Home() {
   deleteCookie('user');
   deleteCookie('token');
-  return <Final />;
-  //return <LoginVoting />;
+  //return <Final />;
+  return <LoginVoting />;
 }
