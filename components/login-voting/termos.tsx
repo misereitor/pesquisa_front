2494e38a03f20a621dd52935ae4eb02cf1d861cc
Misael Miranda <misaelbahia1@hotmail.com>
2026-01-06 17:16:29 -0300

    update termos

--- components/login-voting/termos.tsx
+++ components/login-voting/termos.tsx
@@ -1,85 +1,27 @@
 export default function Termos() {
   return (
     <div className="mx-4 mt-10 overflow-y-auto">
-      <div>
-        <p className="font-light text-justify">
-          Olá! Antes de você participar da pesquisa &quot;Melhores do Ano
-          2025&quot;, é importante que você compreenda e concorde com os termos
-          de uso dos dados. Esse termo é essencial para garantir a transparência
-          e a segurança no tratamento das informações que você fornecerá. Ao
-          prosseguir com a pesquisa e fornecer seus dados, você concorda com os
-          seguintes termos:
-        </p>
-      </div>
-      <ul>
-        <ol className="list-decimal text-justify">
-          <li className="font-bold mt-4 mb-1">Propósito da Coleta de Dados:</li>
-          <p className="font-light">
-            Os dados que você fornecer serão usados exclusivamente para a
-            seleção e divulgação das melhores empresas de Santo Antônio de
-            Jesus, conforme determinado pelas entidades empresariais.
-          </p>
-          <li className="font-bold mt-4 mb-1">Informações Coletadas:</li>
-          <p className="font-light text-justify">
-            Durante a pesquisa, podemos solicitar informações pessoais, como
-            nome, CPF, telefone e outras relevantes para identificação e
-            contato.
-          </p>
-          <li className="font-bold mt-4 mb-1">Uso dos Dados:</li>
-          <p className="font-light text-justify">
-            Seus dados serão utilizados apenas para os propósitos da pesquisa
-            &quot;Melhores do Ano 2025&quot;. Eles não serão compartilhados com
-            terceiros sem sua autorização, exceto quando exigido por lei.
-          </p>
-          <li className="font-bold mt-4 mb-1">Consentimento:</li>
-          <p className="font-light text-justify">
-            Ao fornecer seus dados nesta pesquisa, você está autorizando
-            explicitamente o uso das informações conforme descrito nestes
-            termos.
-          </p>
-          <li className="font-bold mt-4 mb-1">Segurança dos Dados:</li>
-          <p className="font-light text-justify">
-            Implementaremos medidas de segurança para proteger suas informações
-            contra acesso não autorizado ou uso indevido.
-          </p>
-          <li className="font-bold mt-4 mb-1">
-            Acesso e Retificação de Dados:
-          </li>
-          <p className="font-light text-justify">
-            Você tem o direito de acessar, corrigir ou excluir suas informações
-            pessoais a qualquer momento. Basta nos contatar usando as
-            informações fornecidas no final deste documento.
-          </p>
-          <li className="font-bold mt-4 mb-1">
-            Armazenamento e Retenção de Dados:
-          </li>
-          <p className="font-light text-justify">
-            Seus dados serão armazenados de forma segura pelo tempo necessário
-            para os propósitos da pesquisa. Após esse período, eles serão
-            anonimizados ou excluídos, a menos que haja uma obrigação legal de
-            retenção.
-          </p>
-          <li className="font-bold mt-4 mb-1">Uso de dados para o sorteio:</li>
-          <p className="font-light text-justify">
-            Seus dados serão utilizados para realização do sorteio do prêmio
-            para caso responda acima de 70% da pesquisa. Será divulgado apenas o
-            nome do ganhador do sorteio.
-          </p>
-          <li className="font-bold mt-4 mb-1">Alterações nos Termos de Uso:</li>
-          <p className="font-light text-justify">
-            Reservamos o direito de alterar estes termos de uso dos dados.
-            Quaisquer mudanças significativas serão comunicadas de forma clara.
-          </p>
-        </ol>
-      </ul>
-      <div className="mt-9">
-        <p className="font-light text-justify">
-          Se precisar de mais informações ou quiser exercer seus direitos de
-          privacidade de dados, entre em contato conosco em nosso contato
-          {` (75) 3632-8800.`} <br /> Agradecemos sua participação na pesquisa
-          &quot;Melhores do Ano 2025&quot; e pela confiança em nós depositada!
-        </p>
-      </div>
+      <p className="font-light text-justify mb-4">
+        Ao participar da pesquisa, tanto o(a) votado(a) quanto o(a)
+        respondente/votante, conforme o caso, concordam e autorizam as Entidades
+        Empresariais, seus diretores, funcionários e, se necessário, prestadores
+        de serviços contratados para essa atividade, a realizarem o tratamento
+        dos dados necessários para o cadastro e registro dos votos, conforme o
+        disposto no artigo 5º, inciso XIII, da Lei Federal nº 13.709, de 14 de
+        agosto de 2018 (Lei Geral de Proteção de Dados) e no Regulamento do
+        evento "Os Melhores do Ano". Todos os dados serão tratados de acordo com
+        os princípios da boa-fé, finalidade, adequação, necessidade, livre
+        acesso, qualidade dos dados, transparência, segurança, prevenção, não
+        discriminação, e com a responsabilidade de adotar medidas técnicas e
+        administrativas adequadas para proteger os dados pessoais contra acessos
+        não autorizados.
+      </p>
+      <p className="font-light text-justify">
+        As respostas desta pesquisa serão utilizadas exclusivamente para a
+        apuração da pesquisa "Melhores do Ano", promovida pelas Entidades
+        Empresariais de Santo Antônio de Jesus, refletindo apenas a opinião dos
+        votantes.
+      </p>
     </div>
   );
 }
